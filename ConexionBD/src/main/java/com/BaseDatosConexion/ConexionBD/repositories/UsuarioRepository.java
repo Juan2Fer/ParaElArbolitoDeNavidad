fe12524728e4c6dfd84d752ee59b64cef70c1149
Juan Fernando Pèrez Tamayo <juan_perez23232@elpoli.edu.co>
2025-11-14 22:52:05 -0500

    Delete ConexionBD directory

--- ConexionBD/src/main/java/com/BaseDatosConexion/ConexionBD/repositories/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.BaseDatosConexion.ConexionBD.repositories;
-
-import com.BaseDatosConexion.ConexionBD.Model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByUsername(String username);
-}
