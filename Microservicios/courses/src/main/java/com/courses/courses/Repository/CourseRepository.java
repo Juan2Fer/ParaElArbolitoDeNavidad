22d8be169cc40d9cfb4aef0138394e4d27a44950
Juan Fernando Pèrez Tamayo <juan_perez23232@elpoli.edu.co>
2025-11-27 12:33:37 -0500

    Delete Microservicios directory

--- Microservicios/courses/src/main/java/com/courses/courses/Repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.courses.courses.Repository;
-
-import com.courses.courses.Models.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CourseRepository extends JpaRepository< Courses, Long> {
-}
